9c5fddd79b6c8613d8190e51b270b235d8ec26c1
Matt Wagner <mattgwagner@gmail.com>
2014-10-08 16:18:51 -0400

    Add log to JobFactory

--- TaskProcessor/Utilities/JobFactory.cs
+++ TaskProcessor/Utilities/JobFactory.cs
@@ -1,4 +1,5 @@
-﻿using Quartz;
+﻿using Common.Logging;
+using Quartz;
 using Quartz.Spi;
 using System;
 
@@ -6,6 +7,8 @@ namespace TaskProcessor.Utilities
 {
     public class JobFactory : IJobFactory
     {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
         public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
         {
             // Configure your IoC container to return a job instance based on jobType
@@ -13,6 +16,8 @@ namespace TaskProcessor.Utilities
             var jobDetail = bundle.JobDetail;
 
             Type jobType = jobDetail.JobType;
+
+            Log.TraceFormat("Getting instance of job type {0}", jobType);
 
             throw new NotImplementedException();
         }
